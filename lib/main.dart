639eb31c5ddb833226aafca46f0b5d83e49ee028
gurpreet <ng.gurpreet@gmail.com>
2019-03-02 19:44:09 +0530

    products listing and add(in-progress)

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,22 @@
 import 'package:flutter/material.dart';
 
+import './product_manager.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        theme: ThemeData(
+          brightness: Brightness.light,
+          primarySwatch: Colors.deepOrange,
+          accentColor: Colors.purple
+        ),
         home: Scaffold(
-      appBar: AppBar(
-        title: Text('EasyList'),
-      ),
-    ));
+            appBar: AppBar(
+              title: Text('EasyList'),
+            ),
+            body: ProductManager('Food Tester')));
   }
 }
---
+++ lib/product_manager.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+import './products.dart';
+import './product_control.dart';
+
+class ProductManager extends StatefulWidget {
+  final String startingProduct;
+
+  ProductManager(this.startingProduct);
+
+  @override
+  State<StatefulWidget> createState() {
+    return _ProductManagerState();
+  }
+}
+
+class _ProductManagerState extends State<ProductManager> {
+  List<String> _products = [];
+
+  @override
+  void initState() {
+    _products.add(widget.startingProduct);
+    super.initState();
+  }
+
+  void _addProduct(product) {
+    setState(() {
+      _products.add(product);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      Container(margin: EdgeInsets.all(10.0), child: ProductControl(_addProduct)),
+      Products(_products)
+    ]);
+  }
+}
