ddb36d1fe3ed32164a6e8f991b9c7a866fcf15b1
gurpreet <ng.gurpreet@gmail.com>
2019-03-04 21:04:24 +0530

    scrollable product list

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,27 @@
 import 'package:flutter/material.dart';
+//import 'package:flutter/rendering.dart';
 
 import './product_manager.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  // debugPaintSizeEnabled = true;
+  // debugPaintBaselinesEnabled = true;
+  // debugPaintPointersEnabled = true;
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         theme: ThemeData(
-          brightness: Brightness.light,
-          primarySwatch: Colors.deepOrange,
-          accentColor: Colors.purple
-        ),
+            brightness: Brightness.light,
+            primarySwatch: Colors.deepOrange,
+            accentColor: Colors.purple),
         home: Scaffold(
             appBar: AppBar(
               title: Text('EasyList'),
             ),
-            body: ProductManager('Food Tester')));
+            body: ProductManager()));
   }
 }
--- lib/products.dart
+++ lib/products.dart
@@ -5,18 +5,29 @@ class Products extends StatelessWidget {
 
   Products(this.products);
 
+  Widget _buildProductItem(BuildContext context, int index) {
+    return Card(
+      child: Column(
+          children: [Image.asset('assets/food.jpg'), Text(products[index])]),
+    );
+  }
+
+  Widget _buildProductList(){
+    Widget productList;
+    if(products.length > 0){
+      productList = ListView.builder(
+            itemBuilder: _buildProductItem, itemCount: products.length);
+    }
+    else{
+      productList = Center(
+            child: Text('No products found'),
+          );
+    }
+    return productList;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-          children: products
-            .map((element) => Card(
-              child: Column(
-                children: [
-                  Image.asset('assets/food.jpg'),
-                  Text(element)
-                ]
-              ),
-            )
-          ).toList());
+    return _buildProductList();
   }
 }
